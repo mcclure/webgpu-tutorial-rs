4c81ef9b56cf0bfdd41e0cb51fe2fc0f7bd3418a
mcc <andi.m.mcclure@gmail.com>
2023-08-22 16:44:25 -0400

    Rotate pool (currently size 1) of Buffer objects for readback

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ use std::borrow::Cow;
 use std::mem;
 use std::num::NonZeroU64;
 use std::ops::DerefMut;
+use std::sync::{Arc, mpsc};
 use web_time::{Duration, Instant};
 use winit::{
     event::{Event, WindowEvent},
@@ -188,6 +189,9 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
     // FIXME: Make 1024
     const AUDIO_READBACK_BUFFER_LEN:usize = 256;
 
+    // FIXME: Add some way to set this to 0 at runtime (for a "mute").
+    const AUDIO_READBACK_BUFFER_MAX_INFLIGHT:usize = 1;
+
     // Create a quad UV buffer with random reflection. Assumes grid_uv is a multiple of 8.
     fn random_uv_push(grid_uv: &mut [f32]) {
         let mut rng = rand::thread_rng();
@@ -212,7 +216,7 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
         }
     }
 
-    fn generate_resize(size:PhysicalSize<u32>, device: &wgpu::Device, queue: &wgpu::Queue, surface: &wgpu::Surface, swapchain_format: wgpu::TextureFormat, swapchain_capabilities: &wgpu::SurfaceCapabilities, diagonal_vertex_buffer: &wgpu::Buffer, diagonal_index_buffer: &wgpu::Buffer, diagonal_index_len: usize, diagonal_render_pipeline: &wgpu::RenderPipeline, grid_bind_group_layout: &wgpu::BindGroupLayout, default_sampler:&wgpu::Sampler, grid_uniform_buffer:&wgpu::Buffer, rowshift_bind_group_layout:&wgpu::BindGroupLayout, rowshift_uniform_buffer:&wgpu::Buffer, target_bind_group_layout:&wgpu::BindGroupLayout, target_uniform_buffers:&[wgpu::Buffer;TARGET_PASSES], readback_bind_group_layout:&wgpu::BindGroupLayout) -> (u32, f32, u64, u64, wgpu::Texture, wgpu::Buffer, wgpu::Buffer, wgpu::Buffer, u32, wgpu::BindGroup, wgpu::BindGroup, wgpu::util::StagingBelt, wgpu::BufferAddress, wgpu::BufferSize, [wgpu::TextureView;2], [wgpu::BindGroup;TARGET_PASSES], wgpu::Texture, wgpu::TextureView, wgpu::BindGroup, std::sync::Arc<wgpu::Buffer>) {
+    fn generate_resize(size:PhysicalSize<u32>, device: &wgpu::Device, queue: &wgpu::Queue, surface: &wgpu::Surface, swapchain_format: wgpu::TextureFormat, swapchain_capabilities: &wgpu::SurfaceCapabilities, diagonal_vertex_buffer: &wgpu::Buffer, diagonal_index_buffer: &wgpu::Buffer, diagonal_index_len: usize, diagonal_render_pipeline: &wgpu::RenderPipeline, grid_bind_group_layout: &wgpu::BindGroupLayout, default_sampler:&wgpu::Sampler, grid_uniform_buffer:&wgpu::Buffer, rowshift_bind_group_layout:&wgpu::BindGroupLayout, rowshift_uniform_buffer:&wgpu::Buffer, target_bind_group_layout:&wgpu::BindGroupLayout, target_uniform_buffers:&[wgpu::Buffer;TARGET_PASSES], readback_bind_group_layout:&wgpu::BindGroupLayout) -> (u32, f32, u64, u64, wgpu::Texture, wgpu::Buffer, wgpu::Buffer, wgpu::Buffer, u32, wgpu::BindGroup, wgpu::BindGroup, wgpu::util::StagingBelt, wgpu::BufferAddress, wgpu::BufferSize, [wgpu::TextureView;2], [wgpu::BindGroup;TARGET_PASSES], wgpu::Texture, wgpu::TextureView, wgpu::BindGroup, Vec<Arc<wgpu::Buffer>>, mpsc::SyncSender<Arc<wgpu::Buffer>>, mpsc::Receiver<Arc<wgpu::Buffer>>) {
         // Set size
         let config = wgpu::SurfaceConfiguration {
             usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
@@ -412,13 +416,18 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
         // FIXME: Should this be a 1D texture instead of a 1-height 2D texture? Does it even matter?
         let (readback_texture, readback_view) = make_texture_gray(&device, AUDIO_READBACK_BUFFER_LEN as u32, 1, true, true, "readback texture");
 
-        // Read-back buffer (will be used by callback, so has to be refcounted, and callback is 'Send so the Rust typesystem forces an unnecessary atomicity requirement)
-        let readback_buffer = std::sync::Arc::new(device.create_buffer(&wgpu::BufferDescriptor {
-            label: Some("Readback buffer"),
-            size: AUDIO_READBACK_BUFFER_LEN as u64*mem::size_of::<f32>() as u64,
-            mapped_at_creation: false,
-            usage: wgpu::BufferUsages::COPY_DST | wgpu::BufferUsages::MAP_READ // Mutable, can be targeted by copies or by shaders
-        }));
+        // Pool of read-back buffers (buffers will be used by callback, so has to be refcounted, and callback is 'Send so the Rust typesystem forces an unnecessary atomicity requirement)
+        let mut readback_buffers:Vec<Arc<wgpu::Buffer>> = Default::default();
+        for idx in 0..AUDIO_READBACK_BUFFER_MAX_INFLIGHT {
+            readback_buffers.push(Arc::new(device.create_buffer(&wgpu::BufferDescriptor {
+                label: Some(&format!("Readback buffer {}", idx)),
+                size: AUDIO_READBACK_BUFFER_LEN as u64*mem::size_of::<f32>() as u64,
+                mapped_at_creation: false,
+                usage: wgpu::BufferUsages::COPY_DST | wgpu::BufferUsages::MAP_READ // Mutable, can be targeted by copies or by shaders
+            })));
+        }
+        // Using sync_channel because it's theoretically more efficient and we can't overflow it.
+        let (readback_buffer_send, readback_buffer_recv) = mpsc::sync_channel::<Arc<wgpu::Buffer>>(AUDIO_READBACK_BUFFER_MAX_INFLIGHT);
 
         // Bind group for write into read-back texture
         // Can't use texture_bind_group because no parameters
@@ -438,10 +447,10 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
             label: Some("Readback bind group"),
         });
 
-        (diagonal_texture_side, side_y, across_x.try_into().unwrap(), across_y.try_into().unwrap(), diagonal_texture, grid_vertex_buffer, grid_uv_buffer, grid_index_buffer, grid_index.len() as u32, grid_bind_group, rowshift_bind_group, grid_uv_staging_belt, grid_uv_staging_offset, NonZeroU64::new(grid_uv_staging_size).unwrap(), target_views, target_bind_groups, readback_texture, readback_view, readback_bind_group, readback_buffer)
+        (diagonal_texture_side, side_y, across_x.try_into().unwrap(), across_y.try_into().unwrap(), diagonal_texture, grid_vertex_buffer, grid_uv_buffer, grid_index_buffer, grid_index.len() as u32, grid_bind_group, rowshift_bind_group, grid_uv_staging_belt, grid_uv_staging_offset, NonZeroU64::new(grid_uv_staging_size).unwrap(), target_views, target_bind_groups, readback_texture, readback_view, readback_bind_group, readback_buffers, readback_buffer_send, readback_buffer_recv)
     }
 
-    let (mut diagonal_texture_side, mut diagonal_texture_side_ndc, mut diagonal_texture_count_x, mut diagonal_texture_count_y, mut diagonal_texture, mut grid_vertex_buffer, mut grid_uv_buffer, mut grid_index_buffer, mut grid_index_len, mut grid_bind_group, mut rowshift_bind_group, mut grid_uv_staging_belt, mut grid_uv_staging_offset, mut grid_uv_staging_size, mut target_views, mut target_bind_groups, mut readback_texture, mut readback_view, mut readback_bind_group, mut readback_buffer) = generate_resize(size, &device, &queue, &surface, swapchain_format, &swapchain_capabilities, &diagonal_vertex_buffer, &diagonal_index_buffer, diagonal_index_len, &diagonal_render_pipeline, &grid_bind_group_layout, &default_sampler, &grid_uniform_buffer, &rowshift_bind_group_layout, &rowshift_uniform_buffer, &target_bind_group_layout, &target_uniform_buffers, &readback_bind_group_layout);
+    let (mut diagonal_texture_side, mut diagonal_texture_side_ndc, mut diagonal_texture_count_x, mut diagonal_texture_count_y, mut diagonal_texture, mut grid_vertex_buffer, mut grid_uv_buffer, mut grid_index_buffer, mut grid_index_len, mut grid_bind_group, mut rowshift_bind_group, mut grid_uv_staging_belt, mut grid_uv_staging_offset, mut grid_uv_staging_size, mut target_views, mut target_bind_groups, mut readback_texture, mut readback_view, mut readback_bind_group, mut readback_buffers, mut readback_buffer_send, mut readback_buffer_recv) = generate_resize(size, &device, &queue, &surface, swapchain_format, &swapchain_capabilities, &diagonal_vertex_buffer, &diagonal_index_buffer, diagonal_index_len, &diagonal_render_pipeline, &grid_bind_group_layout, &default_sampler, &grid_uniform_buffer, &rowshift_bind_group_layout, &rowshift_uniform_buffer, &target_bind_group_layout, &target_uniform_buffers, &readback_bind_group_layout);
 
     // ------ Data/operations for frame draw ------
 
@@ -513,7 +522,7 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
                 ..
             } => {
                 // Reconfigure the surface with the new size
-                (diagonal_texture_side, diagonal_texture_side_ndc, diagonal_texture_count_x, diagonal_texture_count_y, diagonal_texture, grid_vertex_buffer, grid_uv_buffer, grid_index_buffer, grid_index_len, grid_bind_group, rowshift_bind_group, grid_uv_staging_belt, grid_uv_staging_offset, grid_uv_staging_size, target_views, target_bind_groups, readback_texture, readback_view, readback_bind_group, readback_buffer) = generate_resize(size, &device, &queue, &surface, swapchain_format, &swapchain_capabilities, &diagonal_vertex_buffer, &diagonal_index_buffer, diagonal_index_len, &diagonal_render_pipeline, &grid_bind_group_layout, &default_sampler, &grid_uniform_buffer, &rowshift_bind_group_layout, &rowshift_uniform_buffer, &target_bind_group_layout, &target_uniform_buffers, &readback_bind_group_layout);
+                (diagonal_texture_side, diagonal_texture_side_ndc, diagonal_texture_count_x, diagonal_texture_count_y, diagonal_texture, grid_vertex_buffer, grid_uv_buffer, grid_index_buffer, grid_index_len, grid_bind_group, rowshift_bind_group, grid_uv_staging_belt, grid_uv_staging_offset, grid_uv_staging_size, target_views, target_bind_groups, readback_texture, readback_view, readback_bind_group, readback_buffers, readback_buffer_send, readback_buffer_recv) = generate_resize(size, &device, &queue, &surface, swapchain_format, &swapchain_capabilities, &diagonal_vertex_buffer, &diagonal_index_buffer, diagonal_index_len, &diagonal_render_pipeline, &grid_bind_group_layout, &default_sampler, &grid_uniform_buffer, &rowshift_bind_group_layout, &rowshift_uniform_buffer, &target_bind_group_layout, &target_uniform_buffers, &readback_bind_group_layout);
                 // On macos the window needs to be redrawn manually after resizing
                 window.request_redraw();
             }
@@ -612,59 +621,75 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
                     rpass.draw_indexed(0..target_index_len, 0, 0..1);
                 }
 
-                // Read back final row for audio
-                // Draw final row into 1-pixel-high texture:
-                {
-                    let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-                        label: None,
-                        color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-                            view: &readback_view,
-                            resolve_target: None,
-                            ops: DRAW_OPS,
-                        })],
-                        depth_stencil_attachment: None,
-                    });
-                    rpass.set_pipeline(&readback_pipeline);
-                    rpass.set_vertex_buffer(0, target_vertex_buffer.slice(..));
-                    rpass.set_index_buffer(grid_index_buffer.slice(..), wgpu::IndexFormat::Uint16);
-                    rpass.set_bind_group(0, &readback_bind_group, &[]);
-                    rpass.draw_indexed(0..target_index_len, 0, 0..1);
+                // Real quick see if we have any readback buffers returned
+                while let Ok(readback_buffer) = readback_buffer_recv.try_recv() {
+                    readback_buffers.push(readback_buffer);
                 }
-                encoder.copy_texture_to_buffer(
-                    wgpu::ImageCopyTextureBase {
-                        texture: &readback_texture,
-                        mip_level:0,
-                        origin: wgpu::Origin3d { x:0,y:0,z:0 },
-                        aspect: wgpu::TextureAspect::All
-                    },
-                    wgpu::ImageCopyBuffer {
-                        buffer: &readback_buffer,
-                        layout: wgpu::ImageDataLayout {
-                            offset:0,
-                            bytes_per_row:None, // Not required, one row.
-                            rows_per_image:None, // Not required, texture not cubic.
-                        }
-                    },
-                    wgpu::Extent3d {width:AUDIO_READBACK_BUFFER_LEN as u32, height:1, depth_or_array_layers:1}
-                );
+                // Don't bother with readback if audio is already busy
+                let readback_buffer = readback_buffers.pop();
+                if let Some(ref readback_buffer) = readback_buffer {
+                    // Read back final row for audio
+                    // Draw final row into 1-pixel-high texture:
+                    {
+                        let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                            label: None,
+                            color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                                view: &readback_view,
+                                resolve_target: None,
+                                ops: DRAW_OPS,
+                            })],
+                            depth_stencil_attachment: None,
+                        });
+                        rpass.set_pipeline(&readback_pipeline);
+                        rpass.set_vertex_buffer(0, target_vertex_buffer.slice(..));
+                        rpass.set_index_buffer(grid_index_buffer.slice(..), wgpu::IndexFormat::Uint16);
+                        rpass.set_bind_group(0, &readback_bind_group, &[]);
+                        rpass.draw_indexed(0..target_index_len, 0, 0..1);
+                    }
+
+                    encoder.copy_texture_to_buffer(
+                        wgpu::ImageCopyTextureBase {
+                            texture: &readback_texture,
+                            mip_level:0,
+                            origin: wgpu::Origin3d { x:0,y:0,z:0 },
+                            aspect: wgpu::TextureAspect::All
+                        },
+                        wgpu::ImageCopyBuffer {
+                            buffer: &readback_buffer,
+                            layout: wgpu::ImageDataLayout {
+                                offset:0,
+                                bytes_per_row:None, // Not required, one row.
+                                rows_per_image:None, // Not required, texture not cubic.
+                            }
+                        },
+                        wgpu::Extent3d {width:AUDIO_READBACK_BUFFER_LEN as u32, height:1, depth_or_array_layers:1}
+                    );
+                }
 
                 // Done
                 queue.submit(Some(encoder.finish()));
-                {
+
+                // If we submitted a readback above, queue up to service it when it's done
+                if let Some(readback_buffer) = readback_buffer {
                     let slice = readback_buffer.slice(..);
                     let readback_buffer = readback_buffer.clone();
+                    let readback_buffer_send = readback_buffer_send.clone();
 
                     // The WebGPU spec says this promise resolves successfully only "after the completion of currently-enqueued operations that use 'this'", so this doubles as an on_submitted_work_done for these purposes.
                     slice.map_async(wgpu::MapMode::Read, move |result| {
                         if let Ok(()) = result {
                             let slice = readback_buffer.slice(..);
-                            let range = slice.get_mapped_range();
-                            let row = bytemuck::cast_slice::<u8, f32>(&range);
-                            println!("HAVE MAPPED RANGE! {}", row[0]);
+                            let row = slice.get_mapped_range();
+                            println!("HAVE MAPPED RANGE (of {})! {:#04x} {:#04x} {:#04x} {:#04x}", row.len(), row[0], row[0], row[0], row[0]);
                         }
                         readback_buffer.unmap();
+                        // Drop readback buffer in channel so it can be returned to pool.
+                        // Because there's an inherent cap on objects in the pool, this should never block.
+                        // The ok() safely erases a warning about dropped results (sometimes the other side is closed, and that's fine)
+                        readback_buffer_send.try_send(readback_buffer).ok();
                     });
                 }
+
                 frame.present();
             }
             // The winit docs recommend doing your "state update" in MainEventsCleared and your draw triggering/logic here.
